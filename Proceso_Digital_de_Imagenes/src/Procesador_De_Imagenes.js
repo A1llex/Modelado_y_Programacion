1b7b70ad79c179b04b6842c807f0fa6738ce3c88
A1llex <alexg.fernandeza@ciencias.unam.mx>
2019-09-07 18:51:37 -0500

    Adicion de Autores

--- Proceso_Digital_de_Imagenes/src/Procesador_De_Imagenes.js
+++ Proceso_Digital_de_Imagenes/src/Procesador_De_Imagenes.js
@@ -1,7 +1,17 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *@author  Alex Gerardo Fernandez Aguilar 
+ *@author  Luis Erick Montes Garcia 
+ * La Finalidad de este programa es 
  */
 
+/**
+ * 
+ * @type FiltrosRGB
+ */
+let func = new FiltrosRGB ([arg1, arg2, ...argN], functionBody);
 
+/**
+ * 
+ * @type FiltroMosaico
+ */
+let func = new FiltroMosaico ([arg1, arg2, ...argN], functionBody);
--- Proceso_Digital_de_Imagenes/src/main.js
+++ Proceso_Digital_de_Imagenes/src/main.js
@@ -1,10 +1,16 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *@author  Alex Gerardo Fernandez Aguilar 
+ *@author  Luis Erick Montes Garcia 
+ * La Finalidad de este programa es
  */
 
 //tambien comentario
 
 // aqui sera el main pero sera tambien el controlador
-//el html servira como salida
+
+
+print("Hola mundo");
+
+let name = prompt("What is your name?");
+
+console.log()
