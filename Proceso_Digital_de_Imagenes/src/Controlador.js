25dde0f4f2011e5f2a4729cd8e95046228506674
LErickMG <lemg040798@ciencias.unam.mx>
2019-09-08 02:00:12 -0500

    Respuesta para validacion extension de archivo

--- Proceso_Digital_de_Imagenes/src/Controlador.js
+++ Proceso_Digital_de_Imagenes/src/Controlador.js
@@ -18,16 +18,16 @@ function obtenerImagen(){
 
 	var archivo = gestorArchivo.value;
 	if(validaArchivo(archivo)){
-		alert("bien");
+		alert("El formato del archivo es correcto");
 	}
 	else 
-		alert("mal");
+		alert("Asegurese de que el tipo de su archivo sea jpg, jpeg, png");
 
 }
 
 function validaArchivo(archivo){
 
-	var extPermitidas = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+	var extPermitidas = /(\.jpg|\.jpeg|\.png)$/i;
 	if(extPermitidas.exec(archivo))
 		return true;
 	return false;
