a2151e51eefa6a78606e54f64578915809dbeedf
A1llex <alexg.fernandeza@ciencias.unam.mx>
2019-09-07 20:16:45 -0500

    Filtros RGB Acabados
    aun falta conectarlo con el front end atravez del controlador

--- Proceso_Digital_de_Imagenes/src/Procesador_De_Imagenes.js
+++ Proceso_Digital_de_Imagenes/src/Procesador_De_Imagenes.js
@@ -6,12 +6,39 @@
 
 /**
  * 
- * @type FiltrosRGB
+ * @param {int} rgb Es una bandera Para saber de que color se aplicara el filtro
+ * @param {document} img Sera la informacion de la imgen  a modificar (var c = document.getElementById("myCanvas");
+ * @returns {image} Sera la informacion de la imagen cambiada
  */
-let func = new FiltrosRGB ([arg1, arg2, ...argN], functionBody);
+function FiltrosRGB (rgb,img){
+    var color1;
+    var color2;
+    if(rgb === 0){
+        color1 = 1; 
+        color2 = 2;
+    }else {
+        color1 = 0; 
+    }if(rgb === 1){
+        color2 = 2;
+    }else{
+        color2 = 1;
+    }
+    
+    var ctx = img.getContext("2d");
+    var imgData = ctx.getImageData(0, 0, img.width, img.height);
+    for (var i = 0; i < imgData.data.length; i += 4) { 
+        imgData.data[i+color1] = 0;
+        imgData.data[i+color2] = 0;
+    }
+    return ctx.putImageData(imgData, 0, 0);
+};
 
 /**
  * 
- * @type FiltroMosaico
+ * @param {type} arg1
+ * @param {type} arg2
+ * @returns {undefined}
  */
-let func = new FiltroMosaico ([arg1, arg2, ...argN], functionBody);
+function FiltroMosaico (arg1,arg2){
+    //Body
+};
--- Proceso_Digital_de_Imagenes/src/main.js
+++ Proceso_Digital_de_Imagenes/src/main.js
@@ -9,8 +9,17 @@
 // aqui sera el main pero sera tambien el controlador
 
 
-print("Hola mundo");
+alert("Hola mundo");
 
 let name = prompt("What is your name?");
 
-console.log()
+/* con esto puede obtener del html la imagen pero falta aun
+ * document.getElementById("scream").onload = function() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("scream");
+  ctx.drawImage(img, 0, 0);
+  
+  ctx.putImageData(FiltrosRGB(c), 0, 0);
+};
+ */
